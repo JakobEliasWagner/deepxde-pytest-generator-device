c119b5d68b7a37767a245450df3c9606f5ca2fd6
JakobEliasWagner <jakob@winter-wind.de>
2024-07-09 16:08:55 +0200

    rm unnecessary code

--- tests/test_deepxde.py
+++ tests/test_deepxde.py
@@ -1,13 +1,1 @@
-import torch
 import deepxde as dde
-
-torch.manual_seed(0)
-
-
-def test_deepxde_example():
-    geom = dde.geometry.Interval(0, 1)
-    # Poisson equation: -u_xx = f
-    def equation(x, y, f):
-        dy_xx = dde.grad.hessian(y, x)
-        return -dy_xx - f
-    assert True
